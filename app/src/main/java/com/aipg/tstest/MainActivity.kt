fc959b92d7067954bf7f79146f1f3e31dd593fd6
Mohammad Arif <mdarif75199884913@gmail.com>
2025-02-04 19:26:53 +0530

    f

---
+++ app/src/main/java/com/aipg/tstest/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.aipg.tstest
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.aipg.tstest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding(layoutInflater.inflate(layoutInflater))
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.tv.text = "Colx"
+    }
+}
